217fce4a7bf1b49517307067d6822960f36ca8e1
Aditya Rajvanshi <arajvans@sfu.ca>
2021-08-05 23:20:42 -0700

    Rudimentary error handeling complete

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,32 @@
-(()=>{const e=document.querySelector("#cityName"),t=document.querySelector("#cityTemp"),a=document.querySelector("#main"),n=document.querySelector(".feels"),s=document.querySelector(".wind"),o=document.querySelector(".humidity");function c(c){const i=document.getElementsByClassName("row"),d=document.querySelector("#main-container");console.log(c);let r=c.weather[0].id.toString().substring(0,1);switch(console.log(r),r){case 2:d.style.backgroundImage="url('/dist/imgs/thunderstorm.jpg')";break;default:d.style.backgroundImage="url('/dist/imgs/cloudy.jpg')"}"fade-in1"===d.classList?(d.classList.remove("fade-in1"),d.offsetWidth,d.classList.add("fade-in1")):d.classList.add("fade-in1"),Array.from(i).forEach((e=>{e.classList.contains("fade-in2")?(e.classList.remove("fade-in2"),e.offsetWidth,e.classList.add("fade-in2")):e.classList.add("fade-in2")})),e.textContent=c.name.toUpperCase()+", "+c.sys.country,a.textContent=c.weather[0].main;let l=parseInt(c.main.temp);l/=10,t.textContent=l+"°C";let u=parseInt(c.main.feels_like);u/=10,n.textContent="FEELS LIKE: "+u+"°C",s.textContent="WIND: "+c.wind.speed+"KM/H",o.textContent="HUMIDITY: "+c.main.humidity+"%"}document.querySelector("#search").addEventListener("keyup",(function(e){13===e.keyCode&&(e.preventDefault(),async function(){let e="https://api.openweathermap.org/data/2.5/weather?q="+document.querySelector("#search").value+"&appid=3ec208c61fc3b1d1344597f31d52a230";const t=await fetch(e,{mode:"cors"});c(await t.json())}())})),async function(){const e=await fetch("https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=3ec208c61fc3b1d1344597f31d52a230",{mode:"cors"});404===e.status&&alert("Invalid input"),c(await e.json())}()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("const cityName = document.querySelector('#cityName');\nconst cityTemp = document.querySelector('#cityTemp');\nconst cityCond = document.querySelector('#main');\n\nconst cityFeels = document.querySelector('.feels');\nconst cityWind = document.querySelector('.wind');\nconst cityHumid = document.querySelector('.humidity');\n\nconst searchBar = document.querySelector('#search');\n\nasync function getTempDefault(){\n    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=3ec208c61fc3b1d1344597f31d52a230', {mode: 'cors'});    \n    const temperature = await response.json();\n    displayData(temperature);\n\n}\n\nasync function getTemp(){\n    const search = document.querySelector('#search');\n    let searchURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + search.value + '&appid=3ec208c61fc3b1d1344597f31d52a230';\n    const response = await fetch(searchURL, {mode: 'cors'});\n    if (response.status === 404){\n        throwErrorMsg();\n    }\n    const temperature = await response.json();\n    displayData(temperature);\n}\n\nfunction displayData(data){\n    const weatherInfo = document.getElementsByClassName('row');\n    const background = document.querySelector('#main-container');\n    const details = document.querySelector('.details');\n    \n    console.log(data);\n\n    let cond = data.weather[0].id;\n    let firstDigit = cond.toString().substring(0, 1);\n    console.log(firstDigit)\n\n    switch (firstDigit){\n        case \"2\":\n            background.style.backgroundImage = \"url('/dist/imgs/thunderstorm.jpg')\";\n            break;\n        case \"3\":\n            background.style.backgroundImage = \"url('/dist/imgs/drizzle.jpg')\";\n            break;\n        case \"5\":\n            background.style.backgroundImage = \"url('/dist/imgs/rain.jpg')\";\n            break;\n        case \"6\":\n            background.style.backgroundImage = \"url('/dist/imgs/snow.jpg')\";\n            break;\n        case \"7\":\n            background.style.backgroundImage = \"url('/dist/imgs/fog.jpg')\";\n            break;\n        default:\n            background.style.backgroundImage = \"url('/dist/imgs/cloudy.jpg')\";\n            \n    }\n\n    Array.from(weatherInfo).forEach((div) => {\n        if (div.classList.contains('fade-in2')) {\n            div.classList.remove('fade-in2');\n            div.offsetWidth;\n            div.classList.add('fade-in2');\n        } else {\n            div.classList.add('fade-in2');\n        }\n    });\n\n    cityName.textContent = data.name.toUpperCase() + ', ' + data.sys.country;\n    cityCond.textContent = data.weather[0].main;\n    let tempCorrect = parseInt(data.main.temp);\n    tempCorrect = tempCorrect/10;\n    cityTemp.textContent = tempCorrect + '°C';\n\n    let feelsCorrect = parseInt(data.main.feels_like)\n    feelsCorrect = feelsCorrect/10;\n    cityFeels.textContent = 'FEELS LIKE: ' + feelsCorrect + '°C'\n\n    cityWind.textContent = 'WIND: ' + data.wind.speed + 'KM/H'\n\n    cityHumid.textContent = 'HUMIDITY: ' + data.main.humidity + '%'\n}\n\nfunction throwErrorMsg(){\n    alert(\"Invalid input\");\n}\n\nsearchBar.addEventListener('keyup', function(event){\n    if(event.keyCode === 13){\n        event.preventDefault();\n        getTemp();\n    }            \n})\ngetTempDefault();\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
--- src/index.js
+++ src/index.js
@@ -9,66 +9,54 @@ const cityHumid = document.querySelector('.humidity');
 const searchBar = document.querySelector('#search');
 
 async function getTempDefault(){
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=3ec208c61fc3b1d1344597f31d52a230', {mode: 'cors'});
-    
-    if (response.status === 404){
-        throwErrorMsg();
-    }
-    
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=3ec208c61fc3b1d1344597f31d52a230', {mode: 'cors'});    
     const temperature = await response.json();
     displayData(temperature);
 
 }
-
 
 async function getTemp(){
     const search = document.querySelector('#search');
     let searchURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + search.value + '&appid=3ec208c61fc3b1d1344597f31d52a230';
     const response = await fetch(searchURL, {mode: 'cors'});
+    if (response.status === 404){
+        throwErrorMsg();
+    }
     const temperature = await response.json();
     displayData(temperature);
-    /* let name = temperature.name;
-    cityName.textContent = name.toUpperCase() + ', ' + temperature.sys.country;
-    cityCond.textContent = temperature.weather[0].main;
-    console.log(temperature)
-    tempCorrect = parseInt(temperature.main.temp);
-    tempCorrect = tempCorrect/10;
-    cityTemp.textContent = tempCorrect + '°C';
-
-    let feelsCorrect = parseInt(temperature.main.feels_like)
-    feelsCorrect = feelsCorrect/10;
-    cityFeels.textContent = 'FEELS LIKE: ' + feelsCorrect + '°C'
-
-    cityFeels.textContent = 'FEELS LIKE: ' + temperature.wind.speed + 'KM/H'
-
-    cityHumid.textContent = 'HUMIDITY: ' + temperature.main.humidity + '%' */
 }
 
 function displayData(data){
     const weatherInfo = document.getElementsByClassName('row');
     const background = document.querySelector('#main-container');
+    const details = document.querySelector('.details');
+    
     console.log(data);
 
     let cond = data.weather[0].id;
     let firstDigit = cond.toString().substring(0, 1);
     console.log(firstDigit)
+
     switch (firstDigit){
-        case 2:
+        case "2":
             background.style.backgroundImage = "url('/dist/imgs/thunderstorm.jpg')";
+            break;
+        case "3":
+            background.style.backgroundImage = "url('/dist/imgs/drizzle.jpg')";
+            break;
+        case "5":
+            background.style.backgroundImage = "url('/dist/imgs/rain.jpg')";
+            break;
+        case "6":
+            background.style.backgroundImage = "url('/dist/imgs/snow.jpg')";
+            break;
+        case "7":
+            background.style.backgroundImage = "url('/dist/imgs/fog.jpg')";
             break;
         default:
             background.style.backgroundImage = "url('/dist/imgs/cloudy.jpg')";
+            
     }
-
-    if (background.classList === 'fade-in1'){
-        background.classList.remove('fade-in1');
-        background.offsetWidth;
-        background.classList.add('fade-in1');
-    }
-    else{
-        background.classList.add('fade-in1');
-    }
-    background
 
     Array.from(weatherInfo).forEach((div) => {
         if (div.classList.contains('fade-in2')) {
