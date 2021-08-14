5a70d851be578b36b058567ee0029394b054464a
Aditya Rajvanshi <aditya.rajvanshi18@gmail.com>
2021-08-13 18:07:58 -0700

    fixed kelvin to celsius and some formatting issues

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const cityHumid = document.querySelector('.humidity');
 const searchBar = document.querySelector('#search');
 
 async function getTempDefault(){
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=3ec208c61fc3b1d1344597f31d52a230', {mode: 'cors'});    
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=3ec208c61fc3b1d1344597f31d52a230', {mode: 'cors'});    
     const temperature = await response.json();
     displayData(temperature);
 
@@ -17,7 +17,8 @@ async function getTempDefault(){
 
 async function getTemp(){
     const search = document.querySelector('#search');
-    let searchURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + search.value + '&appid=3ec208c61fc3b1d1344597f31d52a230';
+    let searchURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + search.value + '&units=metric&appid=3ec208c61fc3b1d1344597f31d52a230&=units';
+    console.log(searchURL);
     const response = await fetch(searchURL, {mode: 'cors'});
     if (response.status === 404){
         throwErrorMsg();
@@ -70,13 +71,10 @@ function displayData(data){
 
     cityName.textContent = data.name.toUpperCase() + ', ' + data.sys.country;
     cityCond.textContent = data.weather[0].main;
-    let tempCorrect = parseInt(data.main.temp);
-    tempCorrect = tempCorrect/10;
-    cityTemp.textContent = tempCorrect + '°C';
 
-    let feelsCorrect = parseInt(data.main.feels_like)
-    feelsCorrect = feelsCorrect/10;
-    cityFeels.textContent = 'FEELS LIKE: ' + feelsCorrect + '°C'
+    cityTemp.textContent = Math.round(data.main.temp*10)/10 + '°C';
+
+    cityFeels.textContent = 'FEELS LIKE: ' + Math.round(data.main.feels_like*10)/10 + '°C'
 
     cityWind.textContent = 'WIND: ' + data.wind.speed + 'KM/H'
 
